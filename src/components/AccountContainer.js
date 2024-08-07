7e08887f863265b931049b577594ffb31d5cb021
ernestotieno95@gmail.com <ernest.otieno@student.moringaschool.com>
2024-08-07 08:05:13 +0300

    PUBLISH

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+  const [transactions, setTransactions] = useState([]);
+  const [filteredTransactions, setFilteredTransactions] = useState([]);
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      try {
+        const response = await fetch("http://localhost:8001/transactions");
+        if (!response.ok) {
+          throw new Error("Failed to fetch transactions");
+        }
+        const data = await response.json();
+        setTransactions(data);
+        setFilteredTransactions(data); // Initialize with all transactions
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+
+    fetchTransactions();
+  }, []);
   return (
     <div>
-      <Search />
-      <AddTransactionForm />
-      <TransactionsList />
+      <Search
+        transactions={transactions}
+        setFilteredTransactions={setFilteredTransactions}
+      />
+      <AddTransactionForm
+        transactions={transactions}
+        setTransactions={setTransactions}
+        setFilteredTransactions={setFilteredTransactions}
+      />
+      <TransactionsList transactions={filteredTransactions} />
     </div>
   );
 }
